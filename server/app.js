2b968002397cdc4d88e83241aab78ae0402a7de4
nendale <nahumtefera@gmail.com>
2019-04-11 13:36:46 -0700

    first commit, created schemas and mongodb database

---
+++ server/app.js
@@ -0,0 +1,26 @@
+// Dependencies
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+
+// GraphQL
+const schema = require('./schema/schema');
+
+// connect to mlab database
+mongoose.connect('mongodb://nahom:password123@ds161245.mlab.com:61245/graphql_freecodecamp')
+mongoose.connection.once('open', ()=>{
+  console.log('connected to database')
+})
+
+// Start server
+const app = express();
+let port = 4000;
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}))
+
+app.listen(port, ()=>{
+  console.log(`now listening for requests on port ${port}`)
+})

f11607d0eaa2cfe302c1de814fb14103de71c863
nendale <nahumtefera@gmail.com>
2019-04-11 15:26:57 -0700

    added BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import BookList from './components/BookList'
 
 class App extends Component {
   render() {
     return (
       <div id="main">
-
+        <h1>Graph QL Reading List</h1>
+        <BookList />
+        
       </div>
     );
   }
